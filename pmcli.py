746e933a900592480179b44930c49edca7b00a16
Avanindra Chakraborty <77975448+AvanindraC@users.noreply.github.com>
2021-06-09 23:15:03 +0530

    Add files via upload

--- pmcli.py
+++ pmcli.py
@@ -1,11 +1,38 @@
 import cryptocode
-from caesar_cypher import getTranslatedMessage
 import click
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+MAX_KEY_SIZE = len(SYMBOLS)
+def getKey():
+    key = 0
+    while True:
+        key = 13
+        if (key >= 1 and key <= MAX_KEY_SIZE):
+            return key
+def getTranslatedMessage(mode,message , key):
+    if mode [0] == 'd':
+        key = -key
+    translated = ''
+            
+    for symbol in message:
+        symbolIndex = SYMBOLS.find(symbol)
+        if symbolIndex == -1:
+            translated += symbol
+        else:
+            symbolIndex += key
+            if symbolIndex >= len(SYMBOLS):
+                symbolIndex -= len(SYMBOLS)
+            elif symbolIndex < 0:
+                symbolIndex += len(SYMBOLS)
+                    
+            translated += SYMBOLS[symbolIndex]
+    return translated
+
 @click.group()
-@click.version_option('0.2.1')
+@click.version_option('0.1.2')
 def main():
     """PM: Encrypt, decrypt and save your passwords"""
     pass
+
 @main.command('encrypt', help='"pmcli encrypt" asks for your directory and file to save, then saves your encrypted password')
 @click.argument('text', nargs=1)
 def encrypt(text):
